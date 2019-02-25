211b1b8e0eb08eddc138e69b8e5d7d00cd62502d
Samuel lindquist <Samme_li22@hotmail.com>
2019-02-25 11:35:41 +0100

    more properties, edit deals, customised check out

--- Elmarknad/Scripts/imagepicker.js
+++ Elmarknad/Scripts/imagepicker.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
-    var images = ["../Images/bild4.jpg", "../Images/bild1.jpg", "../Images/bild2.jpg", "../Images/bild3.jpg"]
+    
+    var images = ["../../../Images/bild4.jpg", "../Images/bild1.jpg", "../Images/bild2.jpg","../Images/bild3.jpg"]
     var imageSrc = images[Math.floor(Math.random() * images.length)];
     $(".image-aboutus-banner").css("background", "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(" + imageSrc + ")");
 });
